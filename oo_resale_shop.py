b8c8828515eba5ba9e0ade214f49360edb0a5465
Zoe Neil <zneil@smith.edu>
2024-02-02 11:50:32 -0500

    added inventory

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,7 +1,8 @@
 class ResaleShop:
 
     # What attributes will it need?
-
+    inventory = [] #computer objects will go in here
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
