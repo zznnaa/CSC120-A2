8240f35bf8e5f8fe8b8ced49e89a8c053867eec4
Zoe Neil <zneil@smith.edu>
2024-02-05 20:25:42 -0500

    print inventory method done (i think)

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -40,18 +40,29 @@ class ResaleShop:
     #refurbish computer
     #update computer os
     #print inventory
-    
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item_id in self.inventory:
+                # Print its details
+                print(f'Item ID: {item_id} : {self.inventory[item_id].__dict__}')
+        else:
+            print("No inventory to display.")
 
 def main():
     computer_resale_store = ResaleShop({}, 0)
     computer1 = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    computer2 = Computer("test", "test", 1111, 11, "test", 1111, 1111)
     computer_resale_store.buy(computer1)
+    computer_resale_store.buy(computer2)
     print(computer_resale_store.itemID)
     item_id : int
     item_id = 1
     computer_resale_store.sell(item_id)
+    computer_resale_store.print_inventory()
 
-#computer 1 and item_id placeholders here until I figure out the main.py code
+##computer 1 and item_id placeholders here until I figure out the main.py code
 
 
 if __name__ == "__main__":
